ec95dd8ca44ead2288f7862fdcba30e032010f11
Krishanu Dey <krishanudey99@gmail.com>
2020-05-21 01:11:14 +0530

    adds Frontend

--- src/api/app.ts
+++ src/api/app.ts
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.resolve(__dirname, '..', 'public')));
+app.use(express.static(path.resolve(__dirname, '..', '..', 'public')));
 
 app.use('/api/products', ProductsRouter);
 app.use('/api/cart', CartRouter);
